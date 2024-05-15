ab6adcd2bd02cab146d057593f51e51b2fc75fe4
abhay kumar <abhaykumar2871999@gmail.com>
2024-05-15 14:39:23 +0530

    add alert in program

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function calculateLove(){
   const name1 = document.getElementById("name1").value.trim();
   const name2 = document.getElementById("name2").value.trim();
   
-if (name1==="" || name2===""){
-  alert("please enter both names")
-}
+  if (name1==="" || name2===""){
+    alert("please enter both names")
+  }
 
 }
 
