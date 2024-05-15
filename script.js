d1085e3c308cbf2c6cfdd4462627f40f946b3f61
abhay kumar <abhaykumar2871999@gmail.com>
2024-05-15 14:39:06 +0530

    init JS

---
+++ script.js
@@ -0,0 +1,10 @@
+function calculateLove(){
+  const name1 = document.getElementById("name1").value.trim();
+  const name2 = document.getElementById("name2").value.trim();
+  
+if (name1==="" || name2===""){
+  alert("please enter both names")
+}
+
+}
+
