e5e03cff32019e01594115bfee89536ccf87112c
abhay kumar <abhaykumar2871999@gmail.com>
2024-06-04 17:50:09 +0530

    init JS part

---
+++ script.js
@@ -0,0 +1,7 @@
+const redSlider = document.getElementById('redSlider')
+const greenslider = document.getElementById('greenSlider')
+const blueSlider = document.getElementById('blueSlider')
+
+const redValueSpan = document.getElementById('redValue');
+const greenValueSpan = document.getElementById('greenValue');
+const blueValueSpan = document.getElementById('blueValue');
