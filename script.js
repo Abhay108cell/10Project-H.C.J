693d39ec69f1820aaa21cada249713ce0a3f084d
abhay kumar <abhaykumar2871999@gmail.com>
2024-06-10 18:27:26 +0530

    value change in parse element in  JS part in Tip-Calculator

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ const tipAmountOutput = document.getElementById("tipAmount");
 const totalAmountOutput = document.getElementById("totalAmount");
 const amountPerPersonOutput = document.getElementById("amountPerPerson");
 
+const billAmount = parseFloat(billAmountInput.value);
+const serviceRating = parseFloat(serviceRatingSelect.value);
+const splitCount = parseInt(splitCountInput.value);
+const mealType = mealTypeSelect.value;
 }
 
 
