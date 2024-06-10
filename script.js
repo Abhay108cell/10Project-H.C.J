829694324fd8c36575713e0f9b9cc08acd2310c0
abhay kumar <abhaykumar2871999@gmail.com>
2024-06-10 18:22:59 +0530

    selected all element in  JS part in Tip-Calculator

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function calculateTip(){
-  
+
+const billAmountInput = document.getElementById("billAmount");
+const serviceRatingSelect = document.getElementById("serviceRating");
+const splitCountInput = document.getElementById("splitCount");
+const mealTypeSelect = document.getElementById("mealType");
+
+const tipAmountOutput = document.getElementById("tipAmount");
+const totalAmountOutput = document.getElementById("totalAmount");
+const amountPerPersonOutput = document.getElementById("amountPerPerson");
+
 }
+
+
+document.getElementById('calculateBtn').addEventListener("click", calculateTip)
