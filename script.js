5cc7e79b6cb07ce31671d01736b8c31f915b787e
abhay kumar <abhaykumar2871999@gmail.com>
2024-06-14 17:23:15 +0530

    added variables of week and month in JS part Date-and-Time-Widget

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 let dateContainer = document.querySelector(".date-container");
 let hoursContainer = document.querySelector(".hours");
 let minutesContainer = document.querySelector(".minutes");
-let secondsContainer = document.querySelector(".seconds");0.
+let secondsContainer = document.querySelector(".seconds");
+
+const weekDays =[
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
