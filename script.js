19f107fc44bf623978ec491fe70e79f8d8f0e7b0
abhay kumar <abhaykumar2871999@gmail.com>
2024-06-17 18:45:22 +0530

    function updated in JS part in Get-Unique-Code-Character-Value

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-fucntion detectChar(){
-  
+function detectChar(){
+
+const input = document.getElementById('inputType').value;
+
+if(input){
+
+const uniCodeValue =input.charCodeAt(0);
+const result = `The Unicode Value of ${input} is ${uniCodeValue}`;
+
+}
+
 }
